1de27b3e0915f78735699df738893b0fdc6b5bfe
Hilmy Noerfatih <hilmy.nrf@gmail.com>
2023-06-17 16:33:29 +0700

    Initial Commit

---
+++ FocusApps/FocusAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusAppsApp.swift
+//  FocusApps
+//
+//  Created by Hilmy Noerfatih on 17/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FocusAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
